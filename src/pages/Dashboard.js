bb76c2605cf6637b3f4372c0f4e91048a6a0678a
yoshivn1203 <yoshivn1203@gmail.com>
2022-11-04 00:05:06 +0700

    finish apex chart

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import Chart from 'react-apexcharts';
+import { Link } from 'react-router-dom';
 
 import statusCards from '../assets/JsonData/status-card-data.json';
 import StatusCard from '../components/status-card/StatusCard';
+
+const chartOptions = {
+  series: [
+    {
+      name: 'Online Customers',
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+    },
+    {
+      name: 'Store Customers',
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+    },
+  ],
+  options: {
+    color: ['#6ab04c', '#2980b9'],
+    chart: {
+      background: 'transparent',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    },
+    legend: {
+      position: 'top',
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
+
 const Dashboard = () => {
   return (
     <div>
@@ -18,19 +55,37 @@ const Dashboard = () => {
         </div>
         <div className='col-6'>
           <div className='card full-height'>
-            {/* chart */}
-            {/* <Chart
-                            options={themeReducer === 'theme-mode-dark' ? {
-                                ...chartOptions.options,
-                                theme: { mode: 'dark'}
-                            } : {
-                                ...chartOptions.options,
-                                theme: { mode: 'light'}
-                            }}
-                            series={chartOptions.series}
-                            type='line'
-                            height='100%'
-                        /> */}
+            <Chart
+              // options={themeReducer === 'theme-mode-dark' ? {
+              //     ...chartOptions.options,
+              //     theme: { mode: 'dark'}
+              // } : {
+              //     ...chartOptions.options,
+              //     theme: { mode: 'light'}
+              // }}
+              options={{ ...chartOptions.options, theme: { mode: 'light' } }}
+              series={chartOptions.series}
+              type='line'
+              height='100%'
+            />
+          </div>
+        </div>
+        <div className='col-4'>
+          <div className='card'>
+            <div className='card__header'>
+              <h3>top customers</h3>
+            </div>
+            <div className='card__body'>
+              {/* <Table
+                                headData={topCustomers.head}
+                                renderHead={(item, index) => renderCusomerHead(item, index)}
+                                bodyData={topCustomers.body}
+                                renderBody={(item, index) => renderCusomerBody(item, index)}
+                            /> */}
+            </div>
+            <div className='card__footer'>
+              <Link to='/'>view all</Link>
+            </div>
           </div>
         </div>
       </div>
