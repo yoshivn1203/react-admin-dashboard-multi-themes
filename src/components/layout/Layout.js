cdc63333d2b90b6e159e7411f62491d84c63d610
yoshivn1203 <yoshivn1203@gmail.com>
2022-11-03 16:45:30 +0700

    finish basic topnav layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './layout.css';
 
 import Sidebar from '../sidebar/Sidebar';
+import TopNav from '../topnav/TopNav';
 import MyRoutes from '../Routes';
 
 import { BrowserRouter } from 'react-router-dom';
@@ -13,6 +14,7 @@ const Layout = () => {
       <div className='layout'>
         <Sidebar />
         <div className='layout__content'>
+          <TopNav />
           <div className='layout__content-main'>
             <MyRoutes />
           </div>
